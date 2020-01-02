f505c4ef5d59638520390b59d34b84380864d032
sunxichen <thisissunxichen@outlook.com>
2020-01-02 08:57:22 +0000

    add July, Oct, etc data

--- arxiv_scrape/spiders/arxiv.py
+++ arxiv_scrape/spiders/arxiv.py
@@ -18,9 +18,9 @@ class ArxivSpider(scrapy.Spider):
     
     months = {
         #"Jan":"01"
-         "Feb":"02"
+        # "Feb":"02"
         # "Mar":"03",
-        # "Apr":"04",
+         "Apr":"04",
         # "May":"05",
         # "Jun":"06"
         # "July":"07",
@@ -95,8 +95,8 @@ class Arxiv2Spider(scrapy.Spider):
         # "Jun":"06",
         #"July":"07"
         # "Aug":"08",
-         "Sep":"09"
-        # "Oct":"10",
+        # "Sep":"09"
+         "Oct":"10",
         # "Nov":"11",
         # "Dec":"12"
     }
