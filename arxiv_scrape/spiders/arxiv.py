f0f15fd502c499ac7aa94c0c2df602a12199a483
sunxichen <thisissunxichen@outlook.com>
2020-01-09 13:00:14 +0000

    final

--- arxiv_scrape/spiders/arxiv.py
+++ arxiv_scrape/spiders/arxiv.py
@@ -20,9 +20,9 @@ class ArxivSpider(scrapy.Spider):
         #"Jan":"01"
         # "Feb":"02"
         # "Mar":"03",
-         "Apr":"04",
-        # "May":"05",
-        # "Jun":"06"
+        # "Apr":"04",
+         #"May":"05",
+         "Jun":"06"
         # "July":"07",
         # "Aug":"08",
         # "Sep":"09",
@@ -96,9 +96,9 @@ class Arxiv2Spider(scrapy.Spider):
         #"July":"07"
         # "Aug":"08",
         # "Sep":"09"
-         "Oct":"10",
-        # "Nov":"11",
-        # "Dec":"12"
+        # "Oct":"10",
+       # "Nov":"11",
+         "Dec":"12"
     }
 
     
